0d20930f846ad41971e0836181b4dfeca88f671d
Lucas <lucas.go22@hotmail.com>
2021-12-13 13:11:24 -0300

    Adição das rotas

--- lucas-gomes/src/app/app-routing.module.ts
+++ lucas-gomes/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCompanyComponent } from './add-company/add-company.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "add-company",
+    component: AddCompanyComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- lucas-gomes/src/app/app.module.ts
+++ lucas-gomes/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AddCompanyComponent } from './add-company/add-company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AddCompanyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
